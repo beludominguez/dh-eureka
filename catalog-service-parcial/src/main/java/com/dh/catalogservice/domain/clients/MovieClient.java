81995e6b0f0e6ef02cbaae9f196b4c66cd916a88
beludominguez <re.beludominguez@gmail.com>
2023-03-10 23:40:37 -0300

    update feign url for movies

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/clients/MovieClient.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/clients/MovieClient.java
@@ -3,9 +3,9 @@ package com.dh.catalogservice.domain.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("movie-client")
+@FeignClient("movie-service")
 public interface MovieClient {
 
-    @RequestMapping("/test")
+    @RequestMapping("/movies/test")
     String test();
 }
