6d12d61d42414fde96d84ad351abce361a0d17bb
beludominguez <re.beludominguez@gmail.com>
2023-03-10 23:32:02 -0300

    add eureka to catalog service

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/clients/MovieClient.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/clients/MovieClient.java
@@ -3,7 +3,7 @@ package com.dh.catalogservice.domain.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient
+@FeignClient("movie-client")
 public interface MovieClient {
 
     @RequestMapping("/test")
