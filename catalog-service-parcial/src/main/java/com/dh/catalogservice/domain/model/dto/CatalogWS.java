cee219823d85bd64b7a69b00a2f85e6a38048c11
beludominguez <re.beludominguez@gmail.com>
2023-03-13 18:16:06 -0300

    fix json serialization

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/CatalogWS.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/CatalogWS.java
@@ -1,11 +1,16 @@
 package com.dh.catalogservice.domain.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CatalogWS {
 	private String genre;
 	private List<MovieWS> movies;
--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/MovieWS.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/MovieWS.java
@@ -1,5 +1,12 @@
 package com.dh.catalogservice.domain.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MovieWS {
     private Integer id;
     private String name;
