27fcb1ea678ff6094b27873fd479a63968ee976a
beludominguez <re.beludominguez@gmail.com>
2023-04-10 02:08:25 +0200

    some fixes

--- movie-service-parcial/src/main/resources/data.sql
+++ movie-service-parcial/src/main/resources/data.sql
@@ -1,3 +1,11 @@
+create database dh;
+CREATE TABLE `movie` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `genre` varchar(255) DEFAULT NULL,
+                         `name` varchar(255) DEFAULT NULL,
+                         `url_stream` varchar(255) DEFAULT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 INSERT INTO movie(name, genre, url_stream) VALUES('Dark Shadows', 'Terror', 'https://www.netflix.com/title/70217909');
 INSERT INTO movie(name, genre, url_stream) VALUES('Gremlins', 'Terror', 'https://www.netflix.com/title/562050');
 INSERT INTO movie(name, genre, url_stream) VALUES('Minions', 'Comedy', 'https://www.netflix.com/title/80033394');
